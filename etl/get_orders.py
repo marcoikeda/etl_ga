84b7498523cefe3dcbd202db1e5c543e8f9eac17
Marco <marco.ikeda@gmail.com>
2016-04-05 00:44:07 -0300

    commit

---
+++ etl/get_orders.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from google2pandas import *
+
+def get_orders():
+	#conn = GoogleAnalyticsQuery(secrets='C:/Users/marcoikeda/Downloads/client_secrets.json', token_file_name='C:/Users/marcoikeda/Downloads/analytics.dat')	
+	conn = GoogleAnalyticsQuery(token_file_name='analytics.dat')
+	query = {\
+	   'ids'           : 109089804,
+	   'metrics'       : ['transactionRevenue'],
+	   'dimensions'    : ['transactionId','dimension2', 'dimension3', 'medium', 'source', 'date', 'dateHour'],
+	   'start_date'    : '7daysAgo',
+	   'max_results'   : 1000
+	}
+	df, metadata = conn.execute_query(**query)
+	return df
+
+df = get_orders()
